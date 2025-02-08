9d177acbc969722cd8dfe4fc050559f3ef172b5a
Ben Di Maggio <bdimaggio@gmail.com>
2025-02-08 14:16:52 +0100

    First commit: trying to make it happen with grid and flex.

---
+++ consenttext.js
@@ -0,0 +1,7 @@
+function openPopup() {
+  document.getElementById("aboutPopup").style.display = "block";
+}
+
+function closePopup() {
+  document.getElementById("aboutPopup").style.display = "none";
+}
